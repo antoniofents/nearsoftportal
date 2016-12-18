a8284f60da36c1224d73476bb314795b726fe9a1
afentanes <afentanes@switchfly.com>
2016-12-18 06:02:01 +0000

    adding first emojis

--- public/words.js
+++ public/words.js
@@ -1,43 +1,29 @@
 var drawWords = function (jsonResponse) {
-  // var canvas = document.getElementById('wordsCanvas');
-  // var wordsFalling = [];
-  // var ctx = canvas.getContext('2d');
-  // var gradient = ctx.createLinearGradient(0, 0, 475, 0);
-  // gradient.addColorStop(1, "white");
-  // ctx.fillStyle = gradient;
-  // console.warn(jsonResponse);
-  // var count = 100;
-   var tags = JSON.parse(jsonResponse).tags;
-   console.log(JSON.stringify(jsonResponse).tags); //array
-  //console.log(randomPosition);
+
+    document.getElementById("wordsDiv").innerHTML = "";
+    var emojis = JSON.parse('{"person": "👤", "man": "👨", "woman":"👩","hand": "✋", "happy":"😃" }');
+    
+   var tags = JSON.parse(jsonResponse).description.tags;
+  console.log(tags.length);
   console.warn(tags);
+   var newWord = document.createElement("h2");
+   var emojisDisplay="";
 
   for (var i = 0; i < tags.length; i++) {
 
-    var newWord = document.createElement("h2");
-    newWord.textContent=tags[i].name;
-
-    var element = document.getElementById("wordsDiv");
-    element.appendChild(newWord);
+    if(emojis[tags[i]]== null){
+          emojisDisplay+= tags[i] +"  ";   
+     }else{
+       emojisDisplay+= emojis[tags[i]] +"  "; 
+     }
     
-    //var a = {};
-    // randomly pick one tag
-    //var randomPosition = Math.round(Math.random() * (tags.length -1));
-    
-    //a.tags = tags[randomPosition].name;
-    //a.x = Math.random() * 500; // random X
-    //a.y = Math.random() * 500 - 500; // random Y that is above the screen
-    //a.speed = Math.random() * tags[randomPosition].confidence * 10;
-    //wordsFalling.push(a);
+  
   }
 
-  // setInterval(function() {
-  //   ctx.clearRect(0, 0, 500, 500);
-  //   for (var i = 0; i < count; i = i + 1) {
-  //     var a = wordsFalling[i];
-  //     ctx.fillText(a.tags, a.x, a.y, 16);
-  //     a.y += a.speed; // fall downwards by the speed amount
-  //     if (a.y > 600) a.y = -50; // if off the screen at bottom put back to top
-  //   }
-  // }, 100);  
+
+    newWord.textContent=emojisDisplay;
+
+  document.getElementById("wordsDiv").appendChild(newWord);
+
 }
+
