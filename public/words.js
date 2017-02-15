eab76ba7ba81183f2702e682f89f6684b9d0bcbe
afentanes <afentanes@switchfly.com>
2017-02-15 18:42:29 +0000

    validate message format

--- public/words.js
+++ public/words.js
@@ -5,34 +5,43 @@ var drawWords = function (jsonResponse) {
     
     var jsonMessage=JSON.parse(jsonResponse);
    var tags = jsonMessage.ComputerVision.tags;
-  console.log(tags.length);
-  console.warn(tags);
-   var newWord = document.createElement("span");
-   var emojisDisplay= "Faces Found: " +jsonMessage.ComputerVision.faces.length+"  ";
-  for (var i = 0; i < tags.length; i++) {
+   if (tags === undefined || tags === null) {
+   console.log('not valid response for tags ')
+   }else{
+    console.log(tags.length);
+    console.warn(tags);
+     var newWord = document.createElement("span");
+     var emojisDisplay= "Faces Found: " +jsonMessage.ComputerVision.faces.length+"  ";
+      for (var i = 0; i < tags.length; i++) {
 
-    if(tags[i].confidence>.8){
-      if(emojis[tags[i].name]== null){
-            emojisDisplay+= tags[i].name +"  ";   
-       }else{
-         emojisDisplay+= emojis[tags[i].name] +"  "; 
-       }
-    }
-          
-  }
+        if(tags[i].confidence>.8){
+          if(emojis[tags[i].name]== null){
+                emojisDisplay+= tags[i].name +"  ";   
+           }else{
+             emojisDisplay+= emojis[tags[i].name] +"  "; 
+           }
+        }
+              
+      }
 
- 
+   }
 
-  for(var i = 0; i < jsonMessage.Emotion.length; i++){
-    var emotionFound=jsonMessage.Emotion[i];
-    var emotionProb="default";
-      for(var key in Object.keys(emotionFound.scores)){
-          if (typeof(emotionFound.scores[emotionProb]) == "undefined" ||  emotionFound.scores[Object.keys(emotionFound.scores)[key]]>emotionFound.scores[emotionProb]){
-            emotionProb=Object.keys(emotionFound.scores)[key];
-          }
-      }        
-       emojisDisplay+= emojis[emotionProb] +"  ";
-  }
+   if (jsonMessage.Emotion.length === undefined || jsonMessage.Emotion.length === null) {
+   console.log('not valid response for Emotions ')
+   }else{
+        for(var i = 0; i < jsonMessage.Emotion.length; i++){
+        var emotionFound=jsonMessage.Emotion[i];
+        var emotionProb="default";
+          for(var key in Object.keys(emotionFound.scores)){
+              if (typeof(emotionFound.scores[emotionProb]) == "undefined" ||  emotionFound.scores[Object.keys(emotionFound.scores)[key]]>emotionFound.scores[emotionProb]){
+                emotionProb=Object.keys(emotionFound.scores)[key];
+              }
+          }        
+           emojisDisplay+= emojis[emotionProb] +"  ";
+      }
+   }
+  
+  
 
 
     newWord.textContent=emojisDisplay;
